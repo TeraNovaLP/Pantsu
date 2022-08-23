c435457a6328082d55dd8d9b732138c2878a939c
TeraNovaLP <16338782+TeraNovaLP@users.noreply.github.com>
2022-08-23 22:54:00 +0200

    Init

---
+++ src/StringUtils.ts
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+export default class StringUtils {
+    public static equalsIgnoreCase(str1: string, str2: string): boolean {
+        return str1?.toLowerCase() == str2?.toLowerCase();
+    }
+
+    /**
+     * Checks if the string could be parsed as JSON.
+     */
+    public static validJSON(str: string) {
+        if (!str) return false;
+
+        try {
+            JSON.parse(str);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+}
