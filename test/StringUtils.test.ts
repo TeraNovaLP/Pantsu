72ef72c960d66c5e77e670a5fa16968691158e00
TeraNovaLP <16338782+TeraNovaLP@users.noreply.github.com>
2022-09-04 21:17:29 +0200

    Update StringUtils.test.ts

--- test/StringUtils.test.ts
+++ test/StringUtils.test.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 import test from "ava";
 import { StringUtils } from "../src/index";
 
@@ -7,4 +11,9 @@ test("EqualsIgnoreCase", (t) => {
 
 test("IncludesIgnoreCase", (t) => {
     t.true(StringUtils.includesIgnoreCase("Test - Includes", "inCluDeS"));
+});
+
+test("ValidJSON", (t) => {
+    t.true(StringUtils.validJSON(JSON.stringify({ test: "Test" })));
+    t.false(StringUtils.validJSON("Test"));
 });
